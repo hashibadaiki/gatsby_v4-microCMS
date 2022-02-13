c39a1cb5bb43c9de910587cd8ed50670a2d31d26
hashibadaiki <higemeganeking@gmail.com>
2022-02-14 08:53:36 +0900

    config file書き換え

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,37 +1,51 @@
+require('dotenv').config();
+
 module.exports = {
   siteMetadata: {
     title: `practice-gatsby`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    "gatsby-plugin-emotion",
+    'gatsby-plugin-emotion',
     // {
     //   resolve: "gatsby-plugin-google-analytics",
     //   options: {
     //     trackingId: "",
     //   },
     // },
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/assets/",
+        name: 'images',
+        path: './src/assets/',
       },
-      __key: "assets",
+      __key: 'assets',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
+    },
+    {
+      resolve: 'gatsby-source-microcms',
+      options: {
+        apiKey: process.env.GATSBY_ENV_VAR,
+        serviceId: 'samplehashiba',
+        apis: [
+          {
+            endpoint: 'blog',
+          },
+        ],
+      },
     },
   ],
 };
