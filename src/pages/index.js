092ceebf323cf88306c413ff0a0f25aa8c8e4051
hashibadaiki <higemeganeking@gmail.com>
2022-02-16 16:44:49 +0900

    api 疎通確認実施した

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import react from 'react';
 import { Template } from '../components/templates/Template';
 import { css } from '@emotion/react';
+import { graphql } from 'gatsby';
 
 const sampleStyle = css`
   width: 100%;
   color: red;
 `;
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  console.log(data);
   return (
     <div>
       <div css={sampleStyle}>App</div>
@@ -15,4 +17,15 @@ const IndexPage = () => {
     </div>
   );
 };
-export default IndexPage
+
+export const query = graphql`
+  {
+    allMicrocmsBlog {
+      nodes {
+        title
+      }
+    }
+  }
+`;
+
+export default IndexPage;
